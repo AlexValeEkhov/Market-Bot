d777f9280f6a05117380fd76982e5e60813e6ad2
AlexValeEkhov <118730504+AlexValeEkhov@users.noreply.github.com>
2022-12-29 20:32:03 +0300

    Update db.py

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-engine = create_engine('postgresql://kgyfbgmq:K_OP3EH74Y-hBdik_n039glDvN99D4eM@mouse.db.elephantsql.com/kgyfbgmq')
+engine = create_engine('postgresql:')
 db_session = scoped_session(sessionmaker(bind=engine))
 
 Base = declarative_base()
